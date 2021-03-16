77383034a9bfcba4c2663b2a247e6e3a9e15747b
Solene-Issartel <issartel.solene@gmail.com>
2021-03-16 18:59:25 +0100

    Add prise contact routes

---
+++ model/prisecontact.js
@@ -0,0 +1,52 @@
+const db = require('../db')
+
+
+async function getAllPriseContact() {
+    try {
+        const {rows} = await db.query('select * from public."Prise_Contact";', [])
+        return rows
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function createPriseContact(id_festival, id_societe ,premier, deuxieme,troisieme,statut) {
+    try {
+        const res = await db.query('INSERT INTO public."Prise_Contact" (id_festival_prise_contact, id_societe_prise_contact ,premier_prise_contact, deuxieme_prise_contact,troisieme_prise_contact,statut_prise_contact) VALUES($1,$2, $3, $4, $5, $6);', [id_festival, id_societe ,premier, deuxieme,troisieme,statut])
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function updatePriseContact(id, id_festival, id_societe ,premier, deuxieme,troisieme,statut) {
+    try {
+        const res = await db.query('UPDATE public."Prise_Contact" SET id_festival_prise_contact = $1, id_societe_prise_contact = $2, premier_prise_contact =$3, deuxieme_prise_contact =$4, troisieme_prise_contact = $5, statut_prise_contact=$6  WHERE id_espace = $7;', [id_festival, id_societe ,premier, deuxieme,troisieme,statut,id])
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function getPriseContactById(id_festival, id_societe) {
+    try {
+        const {rows} = await db.query('SELECT * FROM public."Prise_Contact" WHERE id_festival_prise_contact = $1 AND id_societe_prise_contact = $2;', [id_festival, id_societe])
+        return rows
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function getPriseContactByStatus(statut) {
+    try {
+        const {rows} = await db.query('SELECT * FROM public."Prise_Contact" WHERE statut_prise_contact = $1;', [statut])
+        return rows
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+module.exports = {getAllPriseContact,createPriseContact, updatePriseContact, getPriseContactById, getPriseContactByStatus}
---
+++ routes/prisecontact.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+const prisecontact = require("../controller/prisecontactcontroller")
+
+router.get('/', async function(req, res, next) {
+    try {
+        const prisecontacts = await prisecontact.getAllPriseContact()
+        console.log(prisecontacts)
+        res.status(200).json({ message: prisecontacts})
+    }
+    catch (e) {
+        res.status(500).json({ message: "can't load data" });
+    }
+
+});
+
+router.get('festival/:idFestival/societe/:idSociete', async function(req, res, next) {
+    try {
+        const idF = req.params.idFestival
+        const idS = req.params.idSociete
+        const prisecontact = await prisecontact.getPriseContactById(idF, idS)
+        console.log(prisecontact)
+        res.status(200).json({ message: prisecontact})
+    }
+    catch (e) {
+        res.status(500).json({ message: "can't load data" });
+    }
+
+});
+
+router.get('/statut/:statut', async function(req, res, next) {
+    try {
+        const statut = req.params.statut
+        const prisecontact = await prisecontact.getPriseContactByStatus(statut)
+        console.log(prisecontact)
+        res.status(200).json({ message: prisecontact})
+    }
+    catch (e) {
+        res.status(500).json({ message: "can't load data" });
+    }
+
+});
+
+module.exports = router;
