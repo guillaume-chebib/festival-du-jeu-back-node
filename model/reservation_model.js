72850a63cf24d6bbd3a100ca0cd1341776ca5255
LaunoisRemy <remy.launois@gmail.com>
2021-03-17 12:30:33 +0100

    Update : join reservation (pas test)

--- model/reservation_model.js
+++ model/reservation_model.js
@@ -5,7 +5,9 @@ const db = require('../db')
 async function getReservationById(id_reservation) {
     try {
         console.log(id_reservation)
-        const query = 'SELECT * FROM public."Reservation" WHERE id_reservation = $1;'
+        const query = 'SELECT * FROM public."Reservation" INNER JOIN "Festival" F on F.id_festival = "Reservation".id_festival_reservation ' +
+            'INNER JOIN "Societe" S on S.id_societe = "Reservation".id_societe_reservation ' +
+            'WHERE id_reservation = $1;'
         const {rows} = await db.query(query, [id_reservation])
 
         return rows
