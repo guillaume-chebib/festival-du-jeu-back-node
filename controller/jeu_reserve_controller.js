9219be0fb896748ba2b6e0015b7a429ec7e22a38
Solene-Issartel <issartel.solene@gmail.com>
2021-03-17 12:22:47 +0100

    Add 'jeux reservés' model and controller

---
+++ controller/jeu_reserve_controller.js
@@ -0,0 +1,31 @@
+const jeu = require("../model/jeu_reserve_model")
+
+async function getAllJeuReserve(){
+    try{
+        return await jeu.getAllJeuReserve()
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function getJeuReserveByIdFestival(params){
+    try{
+        return await jeu.getJeuReserveByIdFestival(params.id)
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+async function getJeuReserveByIdReservation(params){
+    try{
+        return await jeu.getJeuReserveByIdReservation(params.id)
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+
+module.exports = {getAllJeuReserve, getJeuReserveByIdReservation, getJeuReserveByIdFestival}
