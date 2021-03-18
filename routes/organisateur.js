d6031621959f30747de4b0c2df8d5796881b1608
Guillaume <guillaume.chebib@hotmail.fr>
2021-03-18 20:52:50 +0100

    Add hash + decrypt password Update method organisateur methods in model/controller/routes

--- routes/organisateur.js
+++ routes/organisateur.js
@@ -15,6 +15,17 @@ router.get('/', async function(req, res, next) {
 
 });
 
+router.post('/', async function(req, res, next) {
+    try {
+        const rep = await organisateur_controller.createOrganisateur(req.body)
+        res.status(200).json({ message: "succes"})
+    }
+    catch (e) {
+        res.status(500).json({ message: "impossible d'ajouter l'organisateur" });
+    }
+
+});
+
 router.get('/profil/:id', async function(req, res, next) {
     try {
         const id = req.params.id
