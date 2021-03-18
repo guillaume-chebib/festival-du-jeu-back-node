fbb533a71ca9965bb955277a6061906c1fd9dfba
LaunoisRemy <remy.launois@gmail.com>
2021-03-18 13:04:03 +0100

    Add : Put / post reservation

--- controller/festival_controller.js
+++ controller/festival_controller.js
@@ -17,6 +17,7 @@ async function createFestival(body){
         throw e
     }
 }
+
 async function updateFestival(body){
     try{
         await festival.updateFestival(body.id,body.annee,body.nom)
