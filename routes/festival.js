dc3e94f86636cb3c49eb21fae055869affb30205
Guillaume <guillaume.chebib@hotmail.fr>
2021-03-20 16:07:43 +0100

    Add route to update a festival

--- routes/festival.js
+++ routes/festival.js
@@ -85,4 +85,16 @@ router.get('/:id/zone', async function(req, res, next) {
 
 });
 
+router.put('/:id', async function(req, res, next) {
+    try {
+
+        const festival = await festival_controller.updateFestival(req.body)
+        res.status(200).json({ message: "ok"})
+    }
+    catch (e) {
+        res.status(500).json({ message: "can't load data" });
+    }
+
+});
+
 module.exports = router;
