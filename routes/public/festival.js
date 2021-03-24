802c7ea2935349bffc641637058dba543a0e558b
Guillaume <guillaume.chebib@hotmail.fr>
2021-03-24 12:56:09 +0100

    Refactor API

--- routes/public/festival.js
+++ routes/public/festival.js
@@ -5,7 +5,7 @@ const jeu_reserve_controller = require("../../controller/jeu_reserve_controller"
 router.get('/:id/jeu', async function(req, res, next) {
     try {
         const jeux = await jeu_reserve_controller.getJeuReserveByIdFestivalPublic(req.params)
-        res.status(200).json({message:jeux})
+        res.status(200).json(jeux)
 
     }
     catch (e) {
